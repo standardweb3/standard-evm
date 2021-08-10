7530f789b8b57852ef539d2ed92960522d0cd778
hskang9 <hskang9@github.com>
2021-08-11 02:26:10 +0900

    upgrade polygon token

---
+++ contracts/bridge/polygon/interfaces/IERCProxy.sol
@@ -0,0 +1,5 @@
+interface IERCProxy {
+    function proxyType() external pure returns (uint256 proxyTypeId);
+
+    function implementation() external view returns (address codeAddr);
+}
---
+++ contracts/bridge/polygon/lib/Initializable.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.8.0;
+
+contract Initializable {
+    bool private _initialized = false;
+
+    modifier initializer() {
+        // solhint-disable-next-line reason-string
+        require(!_initialized);
+        _;
+        _initialized = true;
+    }
+
+    function initialized() external view returns (bool) {
+        return _initialized;
+    }
+}

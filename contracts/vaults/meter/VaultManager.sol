b4d68858f3c07c0c4eca49eb3fa41ec20fe2bc23
hskang9 <hskang9@github.com>
2021-08-20 11:45:32 +0900

    remove safemath cuz v0.8.0

--- contracts/vaults/meter/VaultManager.sol
+++ contracts/vaults/meter/VaultManager.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.0;
 import '../../oracle/OracleRegistry.sol';
 import './Vault.sol';
 import './IVaultManager.sol';
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./IV1.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";

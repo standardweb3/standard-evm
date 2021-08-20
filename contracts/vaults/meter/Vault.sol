88970b5e616311187734f1e78b5391729a89d7b8
hskang9 <hskang9@github.com>
2021-08-20 11:45:50 +0900

    remove safemath

--- contracts/vaults/meter/Vault.sol
+++ contracts/vaults/meter/Vault.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.0;
 import '../../oracle/IPrice.sol';
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./IVault.sol";
 import "./IV1.sol";
 import "../../uniswapv2/interfaces/IMTRMarket.sol";

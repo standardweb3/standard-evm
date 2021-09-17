2b8ed167e8298ef48446ac4e7834b06a128c6c5b
Hyungsuk Kang <hskang9@github.com>
2021-09-18 01:09:45 +0900

    update cli commands

--- cli/amm.ts
+++ cli/amm.ts
@@ -1,43 +1,5 @@
 import { task, types } from "hardhat/config";
 import { BigNumber, constants } from "ethers";
-import "./legacy"
-import "./amm"
-import "./token"
-import "./vault"
-import "./synth"
-
-const assert = (condition, message) => {
-    if (condition) return;
-    throw new Error(message);
-  };
-
-task("deploy", "Deploy Standard Multichain Token").setAction(async (args, { ethers }) => {
-    const [deployer] = await ethers.getSigners();
-  
-    // Deploy proxy 
-    console.log(`Deploying Standard Token with the account: ${deployer.address}`);
-  
-    console.log(
-      `Deployer balance: ${ethers.utils.formatEther(
-        await deployer.getBalance()
-      )} ETH`
-    );
-  
-    const Token = await ethers.getContractFactory("Standard");
-    const token = await Token.deploy();
-  
-    console.log("Token address:", token.address);
-  
-    console.log("Mining...");
-    await token.deployed();
-    console.log(
-      `Deployer balance: ${ethers.utils.formatEther(
-        await deployer.getBalance()
-      )} ETH`
-    );
-  });
-
-
 
   task("deploy-amm", "Deploy Standard AMM")
   .addParam("weth", "Address of Wrapped ETH")
---
+++ cli/token.ts
@@ -0,0 +1,28 @@
+import { task, types } from "hardhat/config";
+import { BigNumber, constants } from "ethers";
+
+task("deploy", "Deploy Standard Multichain Token").setAction(async (args, { ethers }) => {
+    const [deployer] = await ethers.getSigners();
+  
+    // Deploy proxy 
+    console.log(`Deploying Standard Token with the account: ${deployer.address}`);
+  
+    console.log(
+      `Deployer balance: ${ethers.utils.formatEther(
+        await deployer.getBalance()
+      )} ETH`
+    );
+  
+    const Token = await ethers.getContractFactory("Standard");
+    const token = await Token.deploy();
+  
+    console.log("Token address:", token.address);
+  
+    console.log("Mining...");
+    await token.deployed();
+    console.log(
+      `Deployer balance: ${ethers.utils.formatEther(
+        await deployer.getBalance()
+      )} ETH`
+    );
+  });

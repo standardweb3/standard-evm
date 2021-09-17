4474589ddd7be2189dc0b670826c2b6cab32f46c
Hyungsuk Kang <hskang9@github.com>
2021-09-18 01:07:39 +0900

    modularize cli

--- cli/legacy.ts
+++ cli/legacy.ts
@@ -1,13 +1,12 @@
 import { task, types } from "hardhat/config";
 import { BigNumber, constants } from "ethers";
 
-
 const assert = (condition, message) => {
     if (condition) return;
     throw new Error(message);
   };
 
-task("deploy", "Deploy Standard Token").setAction(async (args, { ethers }) => {
+task("deploy-stnd-parent", "Deploy Standard Parent Token").setAction(async (args, { ethers }) => {
     const [deployer] = await ethers.getSigners();
   
     console.log(`Deploying Standard Token with the account: ${deployer.address}`);
@@ -32,10 +31,101 @@ task("deploy", "Deploy Standard Token").setAction(async (args, { ethers }) => {
     );
   });
 
-task("deploy-amm", "Deploy Standard AMM").setAction(async (args, {ethers})=> {
-    const [deployer]
-})
-  
+task("revoke-blocked", "Revoke tokens from blocked accounts")
+  .addParam("token", "Address of the protected token contract")
+  .addParam("to", "Address to transfer revoked tokens to")
+  .addParam(
+    "json",
+    'Path to the blocked accounts json. Example: ["0x1234", "0x5678", ...]'
+  )
+  .setAction(async ({ token: tokenAddress, json, to }, {ethers}) => {
+    assert(
+      ethers.utils.isAddress(tokenAddress),
+      `Token address '${tokenAddress}' is invalid.`
+    );
+    assert(ethers.utils.isAddress(to), `Revoke to address '${to}' is invalid.`);
+    const blocked = require(json);
+    for (let account of blocked) {
+      assert(
+        ethers.utils.isAddress(account),
+        `Blocked address '${account}' is invalid.`
+      );
+    }
+    const [sender] = await ethers.getSigners();
+
+    const Token = await ethers.getContractFactory("Standard");
+    const token = await Token.attach(tokenAddress);
+
+    console.log(
+      `Revoking tokens from blocked accounts to ${to}. Transaction sender: ${sender.address}`
+    );
+
+    console.log(
+      `To balance: ${ethers.utils.formatEther(await token.balanceOf(to))}`
+    );
+    console.log(
+      `Sender balance: ${ethers.utils.formatEther(
+        await sender.getBalance()
+      )} ETH`
+    );
+
+    let tx;
+    const batchSize: bigint = 50n;
+    for (let i: bigint = 0n; i <= BigInt(blocked.length) / batchSize; i++) {
+      let entries = blocked.slice(
+        i * batchSize,
+        (i + 1n) * batchSize
+      );
+      tx = await token.connect(sender).revokeBlocked(entries, to);
+      console.log(`Batch ${i + 1n}: ${tx.hash}`);
+    }
+    console.log("Mining...");
+    await (tx && tx.wait());
+
+    console.log(
+      `To balance: ${ethers.utils.formatEther(await token.balanceOf(to))}`
+    );
+    console.log(
+      `Sender balance: ${ethers.utils.formatEther(
+        await sender.getBalance()
+      )} ETH`
+    );
+  });
+
+task("disableProtection", "Manually disable liquidity protection")
+  .addParam("token", "Address of the protected token contract")
+  .setAction(async ({ token: tokenAddress }, {ethers}) => {
+    assert(
+      ethers.utils.isAddress(tokenAddress),
+      `Token address '${tokenAddress}' is invalid.`
+    );
+    const [sender] = await ethers.getSigners();
+
+    const Token = await ethers.getContractFactory("Standard");
+    const token = await Token.attach(tokenAddress);
+
+    console.log(
+      `Disabling liquidity protection with account: ${sender.address}`
+    );
+
+    console.log(
+      `Sender balance: ${ethers.utils.formatEther(
+        await sender.getBalance()
+      )} ETH`
+    );
+
+    const tx = await token.connect(sender).disableProtection();
+    console.log(`${tx.hash}`);
+    console.log("Mining...");
+    await tx.wait();
+
+    console.log(
+      `Sender balance: ${ethers.utils.formatEther(
+        await sender.getBalance()
+      )} ETH`
+    );
+  });
+
   task("deploy-pool", "Deploy Standard Staking pools")
     .addParam("reward", "Address of the reward token contract")
     .addParam("input", "Address of the input token contract")
@@ -118,101 +208,6 @@ task("deploy-amm", "Deploy Standard AMM").setAction(async (args, {ethers})=> {
       console.log(`Last Update Time(timestamp): ${await pool.lastUpdateTime()}`);
       console.log(
         `Period Finishing Time(timestamp): ${await pool.periodFinish()}`
-      );
-    });
-  
-  task("revoke-blocked", "Revoke tokens from blocked accounts")
-    .addParam("token", "Address of the protected token contract")
-    .addParam("to", "Address to transfer revoked tokens to")
-    .addParam(
-      "json",
-      'Path to the blocked accounts json. Example: ["0x1234", "0x5678", ...]'
-    )
-    .setAction(async ({ token: tokenAddress, json, to }, {ethers}) => {
-      assert(
-        ethers.utils.isAddress(tokenAddress),
-        `Token address '${tokenAddress}' is invalid.`
-      );
-      assert(ethers.utils.isAddress(to), `Revoke to address '${to}' is invalid.`);
-      const blocked = require(json);
-      for (let account of blocked) {
-        assert(
-          ethers.utils.isAddress(account),
-          `Blocked address '${account}' is invalid.`
-        );
-      }
-      const [sender] = await ethers.getSigners();
-  
-      const Token = await ethers.getContractFactory("Standard");
-      const token = await Token.attach(tokenAddress);
-  
-      console.log(
-        `Revoking tokens from blocked accounts to ${to}. Transaction sender: ${sender.address}`
-      );
-  
-      console.log(
-        `To balance: ${ethers.utils.formatEther(await token.balanceOf(to))}`
-      );
-      console.log(
-        `Sender balance: ${ethers.utils.formatEther(
-          await sender.getBalance()
-        )} ETH`
-      );
-  
-      let tx;
-      const batchSize: bigint = 50n;
-      for (let i: bigint = 0n; i <= BigInt(blocked.length) / batchSize; i++) {
-        let entries = blocked.slice(
-          i * batchSize,
-          (i + 1n) * batchSize
-        );
-        tx = await token.connect(sender).revokeBlocked(entries, to);
-        console.log(`Batch ${i + 1n}: ${tx.hash}`);
-      }
-      console.log("Mining...");
-      await (tx && tx.wait());
-  
-      console.log(
-        `To balance: ${ethers.utils.formatEther(await token.balanceOf(to))}`
-      );
-      console.log(
-        `Sender balance: ${ethers.utils.formatEther(
-          await sender.getBalance()
-        )} ETH`
-      );
-    });
-  
-  task("disableProtection", "Manually disable liquidity protection")
-    .addParam("token", "Address of the protected token contract")
-    .setAction(async ({ token: tokenAddress }, {ethers}) => {
-      assert(
-        ethers.utils.isAddress(tokenAddress),
-        `Token address '${tokenAddress}' is invalid.`
-      );
-      const [sender] = await ethers.getSigners();
-  
-      const Token = await ethers.getContractFactory("Standard");
-      const token = await Token.attach(tokenAddress);
-  
-      console.log(
-        `Disabling liquidity protection with account: ${sender.address}`
-      );
-  
-      console.log(
-        `Sender balance: ${ethers.utils.formatEther(
-          await sender.getBalance()
-        )} ETH`
-      );
-  
-      const tx = await token.connect(sender).disableProtection();
-      console.log(`${tx.hash}`);
-      console.log("Mining...");
-      await tx.wait();
-  
-      console.log(
-        `Sender balance: ${ethers.utils.formatEther(
-          await sender.getBalance()
-        )} ETH`
       );
     });
   

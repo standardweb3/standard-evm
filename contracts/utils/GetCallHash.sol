8e2fdd7bac9a1349c3a6e1e7a5bc46e7806aa6fa
Hyungsuk Kang <hskang9@gmail.com>
2021-09-17 18:18:54 +0900

    merge uniswapv2 area

---
+++ contracts/utils/GetCallHash.sol
@@ -0,0 +1,27 @@
+pragma solidity =0.6.12;
+import './uniswapv2/UniswapV2Pair.sol';
+
+contract CalHash {
+    function getInitHash() public pure returns(bytes32){
+        bytes memory bytecode = type(UniswapV2Pair).creationCode;
+        return keccak256(abi.encodePacked(bytecode));
+    }
+    
+    // returns sorted token addresses, used to handle return values from pairs sorted in this order
+    function sortTokens(address tokenA, address tokenB) internal pure returns (address token0, address token1) {
+        require(tokenA != tokenB, 'UniswapV2Library: IDENTICAL_ADDRESSES');
+        (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        require(token0 != address(0), 'UniswapV2Library: ZERO_ADDRESS');
+    }
+    
+        // calculates the CREATE2 address for a pair without making any external calls
+    function pairFor(address factory, address tokenA, address tokenB) internal pure returns (address pair) {
+        (address token0, address token1) = sortTokens(tokenA, tokenB);
+        pair = address(uint(keccak256(abi.encodePacked(
+                hex'ff',
+                factory,
+                keccak256(abi.encodePacked(token0, token1)),
+                hex'99c8de1eb60d37a95c365ab0a29e56b15d6a3c09bb02d3c8c64343bcaf6afff0' // init code hash
+            ))));
+    }
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -5,6 +5,12 @@ require("hardhat-gas-reporter");
 // const { start } = require('node:repl');
 const LPS = require("./test/LiquidityProtectionService.json");
 
+const accounts = {
+  mnemonic:
+    process.env.MNEMONIC ||
+    "test test test test test test test test test test test junk",
+};
+
 const assert = (condition, message) => {
   if (condition) return;
   throw new Error(message);
@@ -216,26 +222,271 @@ task("disableProtection", "Manually disable liquidity protection")
   });
 
 module.exports = {
+  defaultNetwork: "local",
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    dev: {
+      default: 1,
+    },
+    alice: {
+      default: 2,
+    },
+    bob: {
+      default: 3,
+    },
+    carol: {
+      default: 4,
+    },
+    dave: {
+      default: 5,
+    },
+    eve: {
+      default: 6,
+    },
+    feeTo: {
+      default: 7,
+    },
+  },
   networks: {
-    target: {
-      url: process.env.RPC_URL,
-      accounts: [process.env.PRIVATE_KEY],
+    localhost: {
+      live: false,
+      saveDeployments: true,
+      tags: ["local"],
     },
-    fork: {
-      url: process.env.ALCHEMY,
+    hardhat: {
+      forking: {
+        enabled: process.env.FORKING === "true",
+        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+        blockNumber: 13000000,
+      },
+      allowUnlimitedContractSize: true,
+      live: false,
+      saveDeployments: true,
+      tags: ["test", "local"],
+      // Solidity-coverage overrides gasPrice to 1 which is not compatible with EIP1559
+      hardfork: process.env.CODE_COVERAGE ? "berlin" : "london",
     },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts,
+      chainId: 3,
+      live: true,
+      saveDeployments: true,
+      tags: ["staging"],
+      gasPrice: 5000000000,
+      gasMultiplier: 2,
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts,
+      chainId: 4,
+      live: true,
+      saveDeployments: true,
+      tags: ["staging"],
+      gasPrice: 5000000000,
+      gasMultiplier: 2,
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts,
+      chainId: 5,
+      live: true,
+      saveDeployments: true,
+      tags: ["staging"],
+      gasPrice: 5000000000,
+      gasMultiplier: 2,
+    },
+    kovan: {
+      url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts,
+      chainId: 42,
+      live: true,
+      saveDeployments: true,
+      tags: ["staging"],
+      gasPrice: 20000000000,
+      gasMultiplier: 2,
+    },
+    fantom: {
+      url: "https://rpcapi.fantom.network",
+      accounts,
+      chainId: 250,
+      live: true,
+      saveDeployments: true,
+      gasPrice: 22000000000,
+    },
+    matic: {
+      url: "https://rpc-mainnet.maticvigil.com",
+      accounts,
+      chainId: 137,
+      live: true,
+      saveDeployments: true,
+    },
+    "matic-testnet": {
+      url: "https://rpc-mumbai.maticvigil.com/",
+      accounts,
+      chainId: 80001,
+      live: true,
+      saveDeployments: true,
+      tags: ["staging"],
+      gasMultiplier: 2,
+    },
+    xdai: {
+      url: "https://rpc.xdaichain.com",
+      accounts,
+      chainId: 100,
+      live: true,
+      saveDeployments: true,
+    },
+    bsc: {
+      url: "https://bsc-dataseed.binance.org",
+      accounts,
+      chainId: 56,
+      live: true,
+      saveDeployments: true,
+    },
+    "bsc-testnet": {
+      url: "https://data-seed-prebsc-2-s3.binance.org:8545",
+      accounts,
+      chainId: 97,
+      live: true,
+      saveDeployments: true,
+      tags: ["staging"],
+      gasMultiplier: 2,
+    },
+    heco: {
+      url: "https://http-mainnet.hecochain.com",
+      accounts,
+      chainId: 128,
+      live: true,
+      saveDeployments: true,
+    },
+    "heco-testnet": {
+      url: "https://http-testnet.hecochain.com",
+      accounts,
+      chainId: 256,
+      live: true,
+      saveDeployments: true,
+      tags: ["staging"],
+      gasMultiplier: 2,
+    },
+    avalanche: {
+      url: "https://api.avax.network/ext/bc/C/rpc",
+      accounts,
+      chainId: 43114,
+      live: true,
+      saveDeployments: true,
+      gasPrice: 470000000000,
+    },
+    "avalanche-testnet": {
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      accounts,
+      chainId: 43113,
+      live: true,
+      saveDeployments: true,
+      tags: ["staging"],
+      gasMultiplier: 2,
+    },
+    harmony: {
+      url: "https://api.s0.t.hmny.io",
+      accounts,
+      chainId: 1666600000,
+      live: true,
+      saveDeployments: true,
+    },
+    "harmony-testnet": {
+      url: "https://api.s0.b.hmny.io",
+      accounts,
+      chainId: 1666700000,
+      live: true,
+      saveDeployments: true,
+      tags: ["staging"],
+      gasMultiplier: 2,
+    },
+    okex: {
+      url: "https://exchainrpc.okex.org",
+      accounts,
+      chainId: 66,
+      live: true,
+      saveDeployments: true,
+    },
+    "okex-testnet": {
+      url: "https://exchaintestrpc.okex.org",
+      accounts,
+      chainId: 65,
+      live: true,
+      saveDeployments: true,
+      tags: ["staging"],
+      gasMultiplier: 2,
+    },
+    arbitrum: {
+      url: "https://arb1.arbitrum.io/rpc",
+      accounts,
+      chainId: 42161,
+      live: true,
+      saveDeployments: true,
+      blockGasLimit: 700000,
+    },
+    "arbitrum-testnet": {
+      url: "https://kovan3.arbitrum.io/rpc",
+      accounts,
+      chainId: 79377087078960,
+      live: true,
+      saveDeployments: true,
+      tags: ["staging"],
+      gasMultiplier: 2,
+    },
+    celo: {
+      url: "https://forno.celo.org",
+      accounts,
+      chainId: 42220,
+      live: true,
+      saveDeployments: true,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
   solidity: {
-    version: "0.8.4",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 999999,
+    compilers: [
+      {
+        version: "0.8.7",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 99999,
+          },
+        },
       },
-    },
+      {
+        version: "0.6.12",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 99999,
+          },
+        },
+      },
+      {
+        version: "0.4.19",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  tenderly: {
+    project: process.env.TENDERLY_PROJECT || "",
+    username: process.env.TENDERLY_USERNAME || "",
   },
   gasReporter: {
-    currency: 'ETH',
-    gasPrice: 67.80
-  }
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    currency: "USD",
+    enabled: process.env.REPORT_GAS === "true",
+    excludeContracts: ["ERC20Mock", "ERC20", "WETH9"],
+  },
 };

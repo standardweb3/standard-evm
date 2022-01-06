861004cd3cba69354e7c893e9b1604f8f34652f8
Hyungsuk Kang <hskang9@github.com>
2022-01-06 18:32:19 +0900

    add URLs for V1

--- contracts/vaults/meter/V1.sol
+++ contracts/vaults/meter/V1.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "./interfaces/IVaultFactory.sol";
 import "./interfaces/IV1.sol";
+import "./interfaces/IVault.sol";
 
 contract V1 is ERC721Enumerable, AccessControl, IV1  {
     // Create a new role identifier for the minter role
@@ -13,9 +14,29 @@ contract V1 is ERC721Enumerable, AccessControl, IV1  {
     bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
     // Vault factory address
     address public factory;
+    // URIs for V1
+    mapping (address => string) URIs;
 
     function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721Enumerable, AccessControl) returns (bool) {
         return super.supportsInterface(interfaceId);
+    }
+
+    function setURI(address collateral_, string memory uri_) public {
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "MTRV1: Caller is not a default admin");
+        URIs[collateral_] = uri_;
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+        address vault = IVaultFactory(factory).getVault(tokenId);
+        address collateral = IVault(vault).collateral();
+        string memory URI = URIs[collateral];
+        if(bytes(URI).length == 0) {
+            // return placeholder URL
+            return URIs[address(0)];
+        } else {
+            return URI;
+        }
     }
 
     constructor(address factory_)

c7f7e8a335d9dda40d1b15270e669050a1ec0513
Hyungsuk Kang <hskang9@github.com>
2022-01-16 06:58:04 +0900

    add minimal proxy

--- contracts/vaults/meter/VaultFactory.sol
+++ contracts/vaults/meter/VaultFactory.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "./Vault.sol";
 import "./interfaces/IVaultFactory.sol";
+
 import "../proxy/transparent/InitializableTransaparentUpgradeableProxy.sol";
 
 contract VaultFactory is AccessControl, IVaultFactory {
@@ -44,12 +45,9 @@ contract VaultFactory is AccessControl, IVaultFactory {
     bytes memory bytecode = type(InitializableTransparentUpgradeableProxy)
       .creationCode;
     bytes32 salt = keccak256(abi.encodePacked("v", gIndex, msg.sender));
-    InitializableTransparentUpgradeableProxy proxy = _createProxy(
-      salt
-    );
-    proxy.initialize(impl, upgrader, "");
+    address proxy = _createClone(impl);
     
-    IVault(address(proxy)).initialize(
+    IVault(proxy).initialize(
       manager,
       gIndex,
       collateral_,
@@ -59,26 +57,72 @@ contract VaultFactory is AccessControl, IVaultFactory {
       v2Factory,
       WETH
     );
-    allVaults.push(address(proxy));
-    return (address(proxy), gIndex);
+    allVaults.push(proxy);
+    return (proxy, gIndex);
   }
 
-  function _createProxy(bytes32 _salt)
-    internal
-    returns (InitializableTransparentUpgradeableProxy)
-  {
-    address payable addr;
-    bytes memory code = type(InitializableTransparentUpgradeableProxy)
-      .creationCode;
+  function _createClone(address target) internal returns (address result) {
+    // convert address to 20 bytes
+    bytes20 targetBytes = bytes20(target);
+
+    // actual code //
+    // 3d602d80600a3d3981f3363d3d373d3d3d363d73bebebebebebebebebebebebebebebebebebebebe5af43d82803e903d91602b57fd5bf3
+
+    // creation code //
+    // copy runtime code into memory and return it
+    // 3d602d80600a3d3981f3
+
+    // runtime code //
+    // code to delegatecall to address
+    // 363d3d373d3d3d363d73 address 5af43d82803e903d91602b57fd5bf3
 
     assembly {
-      addr := create2(0, add(code, 0x20), mload(code), _salt)
-      if iszero(extcodesize(addr)) {
-        revert(0, 0)
-      }
+      /*
+            reads the 32 bytes of memory starting at pointer stored in 0x40
+
+            In solidity, the 0x40 slot in memory is special: it contains the "free memory pointer"
+            which points to the end of the currently allocated memory.
+            */
+      let clone := mload(0x40)
+      // store 32 bytes to memory starting at "clone"
+      mstore(
+        clone,
+        0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000
+      )
+
+      /*
+              |              20 bytes                |
+            0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000
+                                                      ^
+                                                      pointer
+            */
+      // store 32 bytes to memory starting at "clone" + 20 bytes
+      // 0x14 = 20
+      mstore(add(clone, 0x14), targetBytes)
+
+      /*
+              |               20 bytes               |                 20 bytes              |
+            0x3d602d80600a3d3981f3363d3d373d3d3d363d73bebebebebebebebebebebebebebebebebebebebe
+                                                                                              ^
+                                                                                              pointer
+            */
+      // store 32 bytes to memory starting at "clone" + 40 bytes
+      // 0x28 = 40
+      mstore(
+        add(clone, 0x28),
+        0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000
+      )
+
+      /*
+              |               20 bytes               |                 20 bytes              |           15 bytes          |
+            0x3d602d80600a3d3981f3363d3d373d3d3d363d73bebebebebebebebebebebebebebebebebebebebe5af43d82803e903d91602b57fd5bf3
+            */
+      // create new contract
+      // send 0 Ether
+      // code starts at pointer stored in "clone"
+      // code size 0x37 (55 bytes)
+      result := create(0, clone, 0x37)
     }
-    
-    return InitializableTransparentUpgradeableProxy(addr);
   }
 
   function _createImpl() internal {
@@ -108,8 +152,8 @@ contract VaultFactory is AccessControl, IVaultFactory {
   }
 
   function setUpgrader(address upgrader_) public {
-     require(hasRole(DEFAULT_ADMIN_ROLE, _msgSender()), "IA"); // Invalid Access
-     upgrader = upgrader_; 
+    require(hasRole(DEFAULT_ADMIN_ROLE, _msgSender()), "IA"); // Invalid Access
+    upgrader = upgrader_;
   }
 
   function getVault(uint256 vaultId_) external view override returns (address) {
@@ -117,7 +161,8 @@ contract VaultFactory is AccessControl, IVaultFactory {
   }
 
   function vaultCodeHash() external pure override returns (bytes32 vaultCode) {
-    return keccak256(type(InitializableTransparentUpgradeableProxy).creationCode);
+    return
+      keccak256(type(InitializableTransparentUpgradeableProxy).creationCode);
   }
 
   function allVaultsLength() public view returns (uint256) {
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -341,7 +341,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.7",
+        version: "0.8.10",
         settings: {
           optimizer: {
             enabled: true,
